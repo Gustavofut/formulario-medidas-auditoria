eeeadaab02aa4bbeda5bd455158b4e8a292cded4
Gustavofut <gustavo.souza@futcorp.com.br>
2026-01-06 20:08:14 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+const URL_SCRIPT = "COLE_AQUI_A_URL_DO_WEB_APP";
+
+function salvar() {
+  const dados = {
+    responsavel: document.getElementById("responsavel").value,
+    nf: document.getElementById("nf").value,
+    codigo: document.getElementById("codigo").value,
+    tamanho: document.getElementById("tamanho").value,
+    altura: document.getElementById("altura").value,
+    largura: document.getElementById("largura").value
+  };
+
+  if (!dados.codigo || !dados.tamanho) {
+    alert("Preencha Código e Tamanho");
+    return;
+  }
+
+  fetch(URL_SCRIPT, {
+    method: "POST",
+    body: JSON.stringify(dados)
+  })
+  .then(res => res.text())
+  .then(msg => {
+    document.getElementById("mensagem").innerText = msg;
+
+    // LIMPA SOMENTE SKU
+    document.getElementById("codigo").value = "";
+    document.getElementById("tamanho").value = "";
+    document.getElementById("altura").value = "";
+    document.getElementById("largura").value = "";
+    document.getElementById("codigo").focus();
+  });
+}
